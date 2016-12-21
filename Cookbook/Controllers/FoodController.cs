d9539f5e8c9992aab554b0803471c6dc82146fdf
vominhquoc <sliverwestlife4@gmail.com>
2016-12-21 20:14:05 +0700

    change

--- Cookbook/Controllers/FoodController.cs
+++ Cookbook/Controllers/FoodController.cs
@@ -78,7 +78,7 @@ namespace Cookbook.Controllers
         {
 
             //Upload hinh
-            Account account = new Account("hl6ei8egh", "132439745671385", "f7bunvOJ6Fg6Bqynodcx5TRQfaY");
+            Account account = new Account("hvecvcvdb", "535867397653847", "P-dB4fIDvWDPXW2TfvBchQbebno");
             Cloudinary cloudinary = new Cloudinary(account);
 
             var uploadParams = new ImageUploadParams()
